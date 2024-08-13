2ace8c828ae6391f9d72e1dac9cb68ce3dff6e54
setoudie <setoudie@gmail.com>
2024-08-13 10:39:34 +0000

    connection ok

--- prompts/views.py
+++ prompts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from py2neo import Graph
 
 # Create your views here.
+db = Graph('bolt://localhost:7687', auth=("neo4j", "tryagain"))
+
+test_querry = """
+    CREATE (p:Prompt {pid: '1234', content: 'This is a sample prompt'})
+    RETURN p
+"""
+
+db.run(test_querry)
